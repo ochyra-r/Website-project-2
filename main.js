eceeafe07b3cf55efa3dfcee04db57cea6af3469
Rafal Ochyra <rafal.ochyra@gmail.com>
2020-03-27 15:14:58 +0100

    update index.html style.css main.js

--- main.js
+++ main.js
@@ -2,9 +2,17 @@
 
 const iconBurger = document.querySelector('.fa-bars');
 const nav = document.querySelector('nav.burger');
+const navHeight = nav.scrollHeight;
+const divMove = document.querySelector('div.move');
 
 const toggleMenu = function () {
 	nav.classList.toggle('active');
+	// div.classList.toggle('start')
+	if (nav.classList.contains('active')) {
+		divMove.style.top = navHeight + 'px';
+	} else {
+		divMove.style.top = 0;
+	}
 }
 
 iconBurger.addEventListener('click', toggleMenu)
