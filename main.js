e841b151a265f846ed8ec5abbc56e1bba0efc0ed
Rafal Ochyra <rafal.ochyra@gmail.com>
2020-03-29 12:43:16 +0200

    update index.html style.css main.js
    add arrow icon in html and add fuction: scroll to top in JS

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ const toggleMenu = function () {
 }
 
 iconBurger.addEventListener('click', toggleMenu)
+
+const divArrow = document.querySelector('div.scroll-up');
+
+window.addEventListener('scroll', function () {
+	if (window.scrollY > 150) {
+		divArrow.classList.add('active')
+	} else {
+		divArrow.classList.remove('active');
+	}
+})
+
+divArrow.addEventListener('click', function () {
+	// window.scroll(0, 0);
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
+})
