90b030119f3786cc0947f4d79122d86da02ace63
Rafal Ochyra <rafal.ochyra@gmail.com>
2020-04-01 18:29:59 +0200

    add html files, changing footer, update main.js
    add new pages about.html, projects.html and contact.html
    change footer placing in html
    add function in main.js - resize page to change placing footer and .main

--- main.js
+++ main.js
@@ -2,21 +2,41 @@
 
 const iconBurger = document.querySelector('.fa-bars');
 const divMove = document.querySelector('div.move');
+const nav = document.querySelector('nav.burger');
+const footer = document.querySelector('footer');
 
+// Podczas rozwijania menu div.move zmienia swoja wlasciowosc "top"
 const toggleMenu = function () {
-	const nav = document.querySelector('nav.burger');
 	const navHeight = nav.scrollHeight;
 	nav.classList.toggle('active');
-	// div.classList.toggle('start')
 	if (nav.classList.contains('active')) {
 		divMove.style.top = navHeight + 'px';
+		footer.style.bottom = `-${navHeight}px`;
 	} else {
 		divMove.style.top = 0;
+		footer.style.bottom = 0;
 	}
 }
 
 iconBurger.addEventListener('click', toggleMenu)
 
+window.addEventListener('resize', function () {
+	const navHeight = nav.scrollHeight;
+	// Zmiana wartosci top/bottom zaleznie od szerokosci strony w jakiej sie znajdujemy
+	if (window.innerWidth <= 639 && nav.classList.contains('active')) {
+		divMove.style.top = navHeight + 'px';
+		footer.style.bottom = `-${navHeight}px`;
+	} else if (window.innerWidth >= 640 && window.innerWidth < 768 && nav.classList.contains('active')) {
+		divMove.style.top = navHeight + 'px';
+		footer.style.bottom = `-${navHeight}px`;
+	} else if (window.innerWidth >= 768) { // zamykanie menu po przekroczeniu szerokosci okna 768px
+		divMove.style.top = 0;
+		nav.classList.remove('active');
+		footer.style.bottom = 0;
+	}
+})
+
+// Przewijanie stronu do poczatku
 const divArrow = document.querySelector('div.scroll-up');
 
 window.addEventListener('scroll', function () {
