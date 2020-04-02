4c29ecf4d6a865f144e1462cafb274f8715a73bd
Rafal Ochyra <rafal.ochyra@gmail.com>
2020-04-02 13:27:08 +0200

    add form in contact.html and mail.php

---
+++ mail.php
@@ -0,0 +1,13 @@
+		<?php
+		 	// $to      = 'rafal.ochyra@gmail.com';
+			// $name    = $_POST['name'];
+			// $email   = $_POST['email'];
+			// $subject = 'Nowy e-mail od' . $name . '(' . $email . ')';
+			// $message = $_POST['message'];
+			// $headers = 'From: ' . $name . '(' . $email . ')';
+			// $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+			// $headers = 'From: Jonny <jon@example.com>'; 
+			// mail($to, $subject, $message, $headers);
+		
+			echo '<h1>Wiadomość wysłana</h1>'
+		?>
